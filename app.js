d1e0b188ef5f7a3577915704eb43092a572bd05c
martin-marzer <martinvazquez1104@gmail.com>
2021-07-24 06:48:06 -0300

    arreglo de puertos

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Levantando un servidor con Express");
 })
 app.use(express.static("public"));
